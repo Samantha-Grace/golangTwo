7f659dc80c45ae3abee8c14a642ea1b610a0074b
Samantha-Grace <sgrace732@gmail.com>
2021-05-05 15:40:20 -0600

    added test, yml and docker file

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	var tests = []string{
+		"Hello", "World", "John", "Jane", "abc",
+	}
+	for _, input := range tests {
+		if !strings.Contains(Hello(input), input) {
+			t.Errorf("Hello(%q) contains %v, failed", input, input)
+		}
+	}
+}
