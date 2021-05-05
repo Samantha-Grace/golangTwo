922efc105f544df349a1e9e35f6151a5a36b4a7a
Samantha-Grace <sgrace732@gmail.com>
2021-05-05 09:55:01 -0600

    Hello func returns a formatted Welcome Message

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 //url shortener in Go
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func Hello(target string) string {
+	return fmt.Sprintf("Hello, %s!", target)
+}
+func main() {
+	target := "World"
+	if len(os.Args) >= 2 {
+		target = os.Args[1]
+	}
+	fmt.Println(Hello(target))
+}
